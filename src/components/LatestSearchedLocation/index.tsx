a0242c933ca108efa70634ed6c92ca72f828d76b
Malaaaa <37627837+Malaaaa@users.noreply.github.com>
2022-06-03 22:29:07 -0400

    fix :add debounce

--- src/components/LatestSearchedLocation/index.tsx
+++ src/components/LatestSearchedLocation/index.tsx
@@ -1,5 +1,5 @@
 import { Row, Space } from 'antd'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppSelector } from '../../store/hooks';
 import moment from 'moment-timezone';
 
@@ -7,11 +7,16 @@ import moment from 'moment-timezone';
 function LatestSearchedLocation() {
     const [time, setTime] = useState("")
     const locations = useAppSelector((state) => { return state.locations.locations })
-    if (locations.length !== 0) {
-        setInterval(() => {
-            setTime(moment().tz(locations[locations.length - 1].timezone).format('h:mm:ssa z'));
-        }, 1000)
-    }
+    useEffect(() => {
+        // Set a timer after each value change
+        if (locations.length > 0) {
+            const timeout = setInterval(() => {
+                setTime(moment().tz(locations[locations.length - 1].timezone).format('h:mm:ssa z'));
+            }, 1000)
+            return () => clearTimeout(timeout);
+        }
+        // Execute each time after the last useEffect has been processed
+    }, [time, locations])
     return (
         <Row>{locations.length !== 0 &&
             <Space><p>{locations[locations.length - 1].name}</p>
